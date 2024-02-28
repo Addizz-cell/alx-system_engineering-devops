559a384ebdff84e084a71feb34ecb6220fb914ea
fawas iwayemi <iwayemiolawale@gmail.com>
2024-02-28 01:51:02 -0800

    Hard

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-
-input = ARGV[0]
-
-# Regular expression to match the word "School"
-regex = /School/
-
-# Check if the input string matches the regular expression
-if input =~ regex
-  puts input.scan(regex).join
-else
-  puts ""
-end
+puts ARGV[0].scan(/School/).join
